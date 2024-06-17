ba4abe279f0e0c6e60605386fd6e0b3621c5f1da
Martinzerty <martinus.cremers@outlook.fr>
2024-06-17 12:11:57 +0200

    save settings operationnal

--- Memento Mori/Assets/Script/settingsMenu.cs
+++ Memento Mori/Assets/Script/settingsMenu.cs
@@ -7,6 +7,13 @@ public class settingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
     public void SetVolume(float volume){
-        audioMixer.SetFloat("MeExposedParams", volume);
+        Debug.Log(volume);
+        audioMixer.SetFloat("mainVol", volume);
     }
+
+    public void SetFullScreen(bool fullScreen){
+        Screen.fullScreen = fullScreen;
+        Debug.Log($"Full screen is :{Screen.fullScreen}");
+    }
+
 }
